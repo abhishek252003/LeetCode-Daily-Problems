7148a6fde856cae9686aabd80c657da6bfbec672
abhishek kumar chaurasia <123100328+abhishek252003@users.noreply.github.com>
2025-01-16 12:26:04 +0530

    Time: 1 ms (81.85%), Space: 45.2 MB (85.93%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,9 +1,9 @@
 class Solution {
   public int singleNumber(int[] nums) {
     int ans = 0;
-    int n=32;
+   
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < 32; i++) {
       int sum = 0;
       for ( int num : nums)
 
