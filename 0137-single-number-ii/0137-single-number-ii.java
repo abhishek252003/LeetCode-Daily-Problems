545ea25d763d3fcd2842b1c96a9cbd2bb460f002
abhishek kumar chaurasia <123100328+abhishek252003@users.noreply.github.com>
2025-01-16 12:24:42 +0530

    Time: 2 ms (73.08%), Space: 45.3 MB (85.93%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+  public int singleNumber(int[] nums) {
+    int ans = 0;
+    int n=50;
+
+    for (int i = 0; i < n; i++) {
+      int sum = 0;
+      for ( int num : nums)
+
+               sum += num >> i & 1;
+      sum %= 3;
+
+      ans |= sum << i;
+    }
+
+    return ans;
+  }
+}
